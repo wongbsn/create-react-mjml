2377706ad5a8c07963ee7e36eab474eb9b5a5c2a
Benson Wong <wongbsn@gmail.com>
2019-08-22 09:23:17 -0400

    Cleanup

--- server/server.js
+++ server/server.js
@@ -3,7 +3,6 @@ import path from 'path';
 import express from 'express';
 import http from 'http';
 
-// const webpack = require('webpack');
 import webpack from 'webpack';
 import webpackDevMiddleware from 'webpack-dev-middleware';
 import webpackHotMiddleware from 'webpack-hot-middleware';
@@ -11,28 +10,23 @@ import config from '../webpack.config';
 
 import render from './render';
 
-
 const PORT = process.env.PORT || 3000;
-
 const app = express();
 const server = http.createServer(app);
 const router = express.Router();
 
+// Configure webpack with HMR
 config.entry.app.unshift('webpack-hot-middleware/client?reload=true&timeout=1000');
-
-//Add HMR plugin
 config.plugins.push(new webpack.HotModuleReplacementPlugin());
 
 const compiler = webpack(config);
 
-//Enable "webpack-dev-middleware"
+// Use webpack-dev-server with express
 app.use(webpackDevMiddleware(compiler, {
     publicPath: config.output.publicPath
 }));
-
-//Enable "webpack-hot-middleware"
 app.use(webpackHotMiddleware(compiler));
-
+app.use(router);
 
 router.use('^/$', (req, res, next) => {
   fs.readFile(
@@ -63,21 +57,6 @@ router.use('^/$', (req, res, next) => {
 
 router.use(express.static(path.resolve(__dirname, '..', 'public')));
 
-app.use(router);
-
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-
-// reload(app)
-//   .then(function() {
-//     server.listen(PORT, () => {
-//       console.log(`Server running on port ${PORT}`);
-//     });
-//   })
-//   .catch(function(err) {
-//     console.error(
-//       'Reload could not start, could not start server/sample app',
-//       err
-//     );
-//   });
